441dd316677b8a71ceebdf903d36035fed78d3fe
Roland Rabien <roland@rabien.com>
2017-08-06 13:47:45 -0700

    Moving common into submodule

--- common/slDsp.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../JuceLibraryCode/JuceHeader.h"
-
-#include "slUtil.h"
-
-//==============================================================================
-void applyGain (AudioSampleBuffer& buffer, LinearSmoothedValue<float>& gain);
-
