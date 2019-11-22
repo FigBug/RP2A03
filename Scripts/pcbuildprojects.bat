2643335defc6bb4f854e14a2276398e046f1d49b
Roland Rabien <figbug@gmail.com>
2019-11-22 07:19:40 -0800

    Add zip and ftp

--- Scripts/pcbuildprojects.bat
+++ Scripts/pcbuildprojects.bat
@@ -7,7 +7,6 @@ cd "%ROOT%"
 cd Scripts
 mkdir bin
 mkdir bin\win
-mkdir bin\zip
 
 cd "..\plugin\Builds\VisualStudio2017"
 "%MSBUILD_EXE%" RP2A03.sln /p:VisualStudioVersion=15.0 /m /t:Build /p:Configuration=Release64 /p:Platform=x64 /p:PreferredToolArchitecture=x64
@@ -17,6 +16,8 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 cd "%ROOT%\Scripts\bin"
 
-copy "%ROOT%\plugin\Builds\VisualStudio2017\x64\Release64\VST\RP2A03_64b.dll" zip
-copy "%ROOT%\plugin\Builds\VisualStudio2017\Win32\Release\VST\RP2A03_32b.dll" zip
+copy "%ROOT%\plugin\Builds\VisualStudio2017\x64\Release64\VST\RP2A03_64b.dll" .
+copy "%ROOT%\plugin\Builds\VisualStudio2017\Win32\Release\VST\RP2A03_32b.dll" .
 
+7z a RP2A03_Win.zip RP2A03_32b.dll RP2A03_64b.dll
+copy RP2A03_Win.zip win
